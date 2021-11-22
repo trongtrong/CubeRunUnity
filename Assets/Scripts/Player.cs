e67ecb75a4e3b4b78a838fc363c82ff1a1cc5ba9
tronghome <trongnv@9pay.vn>
2021-11-22 23:35:49 +0700

    First project unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rigidBody;
+    public float jumpForce;
+    private bool isGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isJumpKeyPressed = Input.GetKeyDown(KeyCode.Space);
+        if (isJumpKeyPressed && isGround)
+        {
+            //Debug.Log("Da an phim space"); 
+            rigidBody.AddForce(Vector2.up * jumpForce);
+            isGround = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+
+
+}
